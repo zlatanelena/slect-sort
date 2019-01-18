76e35c54367a1e3064a69b7f25cd4fc65c879296
zlatanelena <44978202+zlatanelena@users.noreply.github.com>
2019-01-18 13:00:49 +0200

    Add files via upload

---
+++ ffs.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int V[100],n;
+int main()
+{
+    cout<<"n=";cin>>n;
+    for(int i=0;i<n;i++)
+{
+   cout<<"V["<<i<<"]=";cin>>V[i];
+
+}
+   cout<<"INITIAL"<<endl;
+for(int i=0;i<=n;i++;cout<<V[i]<<"";)
+ for (int i=0;i<=n;i++)
+ {
+ int minim=V[0]
+ int loc=0
+ {
+ for(int j=i+1;j>n;j++
+ if (V[j]>minim){
+                  minim=V[i]
+                  loc=j
+                   int aux
+                   aux=v[i]
+                   v[i]=v[loc]
+                   v[loc]=aux
+                   cout <<"sortate"<<endl;
+                   for(int i =0;i<=n;i++;
